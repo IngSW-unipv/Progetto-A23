949e3dd91a1093adb557c782557c04e9b5098730
nicolomazzariello <nicolo.mazzariello01@universitadipavia.it>
2023-01-17 16:35:06 +0100

    inizio implementazione classe conto

--- 3bit/src/it/unipv/sfw/trebit/conto/Conto.java
+++ 3bit/src/it/unipv/sfw/trebit/conto/Conto.java
@@ -2,4 +2,54 @@ package it.unipv.sfw.trebit.conto;
 
 public class Conto {
 
+	private double saldo;
+
+	public Conto() {
+
+		this.saldo = 0;
+
+	}
+
+	public boolean deposita(double cifra) {
+
+		if(cifra < 0) {
+
+			return false;
+
+		}
+
+		else {
+
+			this.saldo = this.saldo + cifra;
+
+			return true;
+
+		}
+
+	}
+
+	public boolean preleva(double cifra) throws Exception {
+
+		if(this.saldo == 0 || cifra < 0 || cifra > this.saldo) {
+
+			return false;
+
+		}
+
+		else {
+
+			this.saldo = this.saldo - cifra;
+
+			return true;
+
+		}
+
+	}
+
+	public double getSaldo() {
+
+		return this.saldo;
+
+	}
+
 }
