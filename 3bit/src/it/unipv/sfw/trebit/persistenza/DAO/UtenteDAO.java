025c746a2c4e0eb6cd1bd0cc25f737af670760b3
Cristian <cristian.senzah01@gmail.com>
2023-02-09 17:48:43 +0100

    Persistenza

---
+++ 3bit/src/it/unipv/sfw/trebit/persistenza/DAO/UtenteDAO.java
@@ -0,0 +1,15 @@
+package it.unipv.sfw.trebit.persistenza.DAO;
+
+import java.sql.Connection;
+
+public class UtenteDAO {
+
+		private String schema;
+		private Connection conn;
+		
+		public UtenteDAO() {
+			super();
+			this.schema = "PROVA";
+		}
+		
+}
