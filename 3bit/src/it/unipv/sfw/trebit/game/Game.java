b1273b3a75db4d916427a1a630cdafac1bdcc390
Luca <luca.iacopino01@universitadipavia.it>
2023-02-13 00:53:39 +0100

    solved errors class Game

--- 3bit/src/it/unipv/sfw/trebit/game/Game.java
+++ 3bit/src/it/unipv/sfw/trebit/game/Game.java
@@ -1,50 +1,17 @@
 package it.unipv.sfw.trebit.game;
 
+import it.unipv.sfw.trebit.game.games.SlotMachine;
+import it.unipv.sfw.trebit.game.games.WheelOfFortune;
+import it.unipv.sfw.trebit.game.multiplier.MultiplierContext;
+
 public class Game {
 	private double multiplier;
 	private int win;
-
-		
 	
 	
-	/*
-	public play(double bet, int chosenGame)
-	
-	multiplier=v.getMultiplier();
-	//istanze dei due giochi   soluzione 1    S R		(non mi piace, in particolare se faccio il metodo choseGame)
-	
-	switch(chosenGame) {
-	case 1: 
-			//istanza gioco scelto soluzione 2    S		(forse è meglio così)
-			win=S.turn();
-			return bet-win*multiplier;
-			break;					//forse non serve il break dato che prima c'è un return e quindi esce a prescindere
-			
-	case 2: 
-			//istanza gioco scelto soluzione 2     R	(forse è meglio così)
-			win=R.turn();
-			return win*multiplier-bet;
-			break;					//forse non serve il break dato che prima c'è un return e quindi esce a prescindere
-	}
-	
-	
-	//in caso di aggiunta di un gioco basta aggiungere un case
-	//forse conviene creare un metodo choseGame che abbia al suo interno solo la switch così non metto tutto insieme
-	//e l'aggiunta di un gioco non necessiterà di troppe modifiche (quantità di linee di codice)
-	
-	*/
-	
-	
-	
-	
-	public double play(double bet, int chosenGame, int extra) {			//metodo chiamato da Casino!!!
-		
-	//forse non servono	
-	//	super();								//non sono molto convinto si faccia così
-	//	this.bet=bet;							//non sono molto convinto si faccia così, però serve 
-	//	this.chosenGame=chosenGame;				//non sono molto convinto si faccia così, però serve 
-	
-		MultiplierContext m = new MultiplierContext();   //da capire bene come metterlo giù e se metterlo qui
+	public double play(double bet, int chosenGame, int extra) {		
+		//istanza di m (MultiplierContext)
+		MultiplierContext m = new MultiplierContext();   
 		multiplier=m.getMultiplier();
 		
 		win=choseGame(chosenGame, extra);
@@ -60,13 +27,13 @@ public class Game {
 			//istanza di s (SlotMachine)
 			SlotMachine s=new SlotMachine();
 			return s.turn(e);
-			break;					//forse non serve il break dato che prima c'è un return e quindi esce a prescindere
 		case 2:
 			//istanza di W (WheelOfFortune)
 			WheelOfFortune w=new WheelOfFortune();
 			return w.turn(e);
-			break;					//forse non serve il break dato che prima c'è un return e quindi esce a prescindere
 		}
+		
+		return 0;
 	}
 }
 
