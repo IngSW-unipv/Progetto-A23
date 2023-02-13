6ff75857b3ced83f635c5dff93a7c24c1d7d4220
Luca <luca.iacopino01@universitadipavia.it>
2023-02-13 19:50:10 +0100

    Creation class Casino(incomplete) with some methods

---
+++ 3bit/src/it/unipv/sfw/trebit/casino/Casino.java
@@ -0,0 +1,42 @@
+package it.unipv.sfw.trebit.casino;
+
+import it.unipv.sfw.trebit.game.Game;
+import it.unipv.sfw.trebit.conto.Conto;
+import it.unipv.sfw.trebit.database.*;
+
+public class Casino {
+	private double[] result;
+	private double outcome;
+	
+	
+	public boolean setConto(/*argomenti per comunicare con il database (credo siano username e password)*/) {
+		//istanza database 
+		saldo=//metodo di database che ritorna il saldo dell'utente
+		Conto c=new Conto(double saldo);
+		return true;
+	}
+	
+	public double[] playGame(double bet, int chosenGame, int extra) {
+		
+//		if(c.getSaldo<bet)
+//			throw exception
+		
+		
+		Game g=new Game();
+		result=g.play(bet, chosenGame, extra);
+		outcome=result[0];
+		
+		
+		//al posto della if:	outcome>0?c.deposita(outcome):c.preleva(outcome*(-1));		
+		if(outcome>0)
+			c.deposita(outcome);
+		else
+			c.preleva(outcome*(-1));
+		
+				
+		return result;
+			
+		
+	}
+
+}
