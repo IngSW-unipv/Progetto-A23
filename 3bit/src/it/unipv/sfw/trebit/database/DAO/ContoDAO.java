f1a80819161bc47f38ccc5560538450b4069f16d
Cristian <cristian.senzah01@gmail.com>
2023-02-27 14:35:01 +0100

    Metodi preleva e deposita ContoDAO

--- 3bit/src/it/unipv/sfw/trebit/database/DAO/ContoDAO.java
+++ 3bit/src/it/unipv/sfw/trebit/database/DAO/ContoDAO.java
@@ -3,6 +3,7 @@ package it.unipv.sfw.trebit.database.DAO;
 import java.sql.*;
 
 import it.unipv.sfw.trebit.database.DBConn;
+import it.unipv.sfw.trebit.model.Conto;
 import it.unipv.sfw.trebit.model.account.Utente;
 
 public class ContoDAO {
@@ -29,6 +30,43 @@ public class ContoDAO {
 			saldo = 0;
 		}
 		return saldo;
+	}
+	
+	public boolean preleva(Utente u, double amount) throws SQLException {
+		conn = DBConn.startConnection(conn, schema);
+		
+		String query = "UPDATE CONTO SET SALDO = SALDO - ? WHERE USERNAME = ?;";
+		PreparedStatement st1 = conn.prepareStatement(query);
+		st1.setDouble(1, amount);
+		st1.setString(2, u.getUsername());
+		return true;
+	}
+	
+	public boolean deposita(Utente u, double amount) throws SQLException {
+		conn = DBConn.startConnection(conn, schema);
+		
+		String query = "UPDATE CONTO SET SALDO = SALDO + ? WHERE USERNAME = ?;";
+		PreparedStatement st1 = conn.prepareStatement(query);
+		st1.setDouble(1, amount);
+		st1.setString(2, u.getUsername());
+		return true;
+	}
+	
+	public Conto getContoByUtente(Utente u) throws SQLException {
+		conn = DBConn.startConnection(conn, schema);
+		Conto c;
+		
+		String query = "SELECT * FROM CONTO WHERE USERNAME = ?;";
+		PreparedStatement st1 = conn.prepareStatement(query);
+		st1.setString(1, u.getUsername());
+		ResultSet result = st1.executeQuery();
+		if(result.next()) {
+			c = new Conto(result.getString("USERNAME"), result.getDouble("SALDO"));
+		}
+		else {
+			c = new Conto("Missing", 0);
+		}
+		return c;
 	}
 	
 }
